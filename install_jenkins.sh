35e8121e5835cdde5933d5d1b3de8c1d1eaeb32d
Sriharsha Kalluru <Sriharsha.Kalluru.glu.com>
2017-12-05 12:45:30 +0530

    Updating jenkins

---
+++ install_jenkins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Source Common Functions
+curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh" >/tmp/common-functions.sh
+#source /root/scripts/common-functions.sh
+source /tmp/common-functions.sh
+
+## Checking Root User or not.
+CheckRoot
+
+## Checking SELINUX Enabled or not.
+CheckSELinux
+
+## Checking Firewall on the Server.
+CheckFirewall
+
+### Install Jenkins
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install jenkins java -y
