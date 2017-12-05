c400b27dab7356eea63f1099c12c8892b2a08600
Sriharsha Kalluru <Sriharsha.Kalluru.glu.com>
2017-12-05 12:46:06 +0530

    Updating jenkins

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -18,3 +18,5 @@ CheckFirewall
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install jenkins java -y
+systemctl enable jenkins
+systemctl start jenkins
